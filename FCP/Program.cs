a8e02216b87a7137ea35d4c3d8469115c99289bf
rony.e.mansor <rony.e.mansor@gmail.com>
2025-06-17 22:23:40 +0300

    create the main form with all elements needed

--- FCP/Program.cs
+++ FCP/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +18,7 @@ namespace FCP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
---
+++ FCP/Views/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// This is the main code-behind file for your form.
+// All the logic for button clicks and other events will go here.
+
+namespace FCP
+{
+    public partial class MainForm : Form
+    {
+        // Flag to track the paused state of an operation
+        private bool isPaused = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            // Initially hide the password text box
+            txtPassword.Visible = false;
+        }
+
+        // Event handler for the "Add Files" button
+        private void btnAddFiles_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement logic to open a FilePickerDialog
+            // and add selected file paths to the ListView.
+            MessageBox.Show("Add Files button clicked!");
+        }
+
+        // Event handler for the "Add Folder" button
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement logic to open a FolderBrowserDialog
+            // and add the folder path to the ListView.
+            MessageBox.Show("Add Folder button clicked!");
+        }
+
+        // Event handler for the "Remove Selected" button
+        private void btnRemoveSelected_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement logic to remove the selected items
+            // from the ListView.
+            MessageBox.Show("Remove Selected button clicked!");
+        }
+
+        // Event handler for the "Compress" button
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            // TODO: This is where the main compression logic will be triggered.
+            // 1. Get the list of files/folders from the ListView.
+            // 2. Ask the user for a save location for the archive.
+            // 3. Get the selected algorithm (Huffman/Shannon-Fano).
+            // 4. If password protection is enabled, get the password.
+            // 5. Start the compression on a background thread (using async/await and a CancellationTokenSource).
+            // 6. Update the progress bar and status labels.
+            MessageBox.Show("Compress button clicked!");
+        }
+
+        // Event handler for the "Extract" button
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            // TODO: This is where the main extraction logic will be triggered.
+            // 1. Ask the user to select an archive file.
+            // 2. Display the archive contents in the ListView.
+            // 3. Ask the user for an extraction destination folder.
+            // 4. If the archive is encrypted, prompt for the password.
+            // 5. Start extraction on a background thread.
+            MessageBox.Show("Extract button clicked!");
+        }
+
+        // Event handler for the "Cancel" button in the status panel
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement logic to cancel the ongoing operation.
+            // This will typically involve calling .Cancel() on a CancellationTokenSource.
+            MessageBox.Show("Cancel button clicked!");
+        }
+
+        // Event handler for the combined Pause/Resume button
+        private void btnPauseResume_Click(object sender, EventArgs e)
+        {
+            isPaused = !isPaused; // Toggle the state
+
+            if (isPaused)
+            {
+                btnPauseResume.Text = "Resume";
+                // TODO: Implement logic to pause the operation.
+                // This could involve using a ManualResetEventSlim to block the background thread.
+                MessageBox.Show("Operation Paused!");
+            }
+            else
+            {
+                btnPauseResume.Text = "Pause";
+                // TODO: Implement logic to resume the operation.
+                // This would signal the ManualResetEventSlim to unblock the background thread.
+                MessageBox.Show("Operation Resumed!");
+            }
+        }
+
+        // Event handler for when the "Encrypt with Password" checkbox is changed
+        private void chkPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            // Show or hide the password a box based on the checkbox state.
+            txtPassword.Visible = chkPassword.Checked;
+        }
+    }
+}
