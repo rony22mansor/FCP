7c1793ba76bf003eefa18fd18c21283d6c365a2e
MarioAndra <marioandrawos75@gmail.com>
2025-06-27 15:05:33 +0300

    Separate Encryption & Decryption logic from mainForm

--- FCP/Helpers/DecryptionHelper.cs
+++ FCP/Helpers/DecryptionHelper.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿using FCP;
+using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Windows.Forms;
 
 public static class DecryptionHelper
 {
     public static byte[] DecryptWithPassword(byte[] encryptedData, string password)
     {
         if (encryptedData.Length < 32)
-        {
             throw new ArgumentException("The encrypted data is too short to contain salt and IV.");
-        }
 
-        // استخراج الـ Salt والـ IV من بداية البيانات
         byte[] salt = new byte[16];
         byte[] iv = new byte[16];
         Buffer.BlockCopy(encryptedData, 0, salt, 0, salt.Length);
         Buffer.BlockCopy(encryptedData, salt.Length, iv, 0, iv.Length);
 
-        // باقي البيانات المشفرة
         int encryptedContentOffset = salt.Length + iv.Length;
         int encryptedContentLength = encryptedData.Length - encryptedContentOffset;
         byte[] cipherBytes = new byte[encryptedContentLength];
@@ -38,5 +37,30 @@ public static class DecryptionHelper
                 return ms.ToArray();
             }
         }
+    }
+
+   
+    public static byte[] HandleDecryptionIfNeeded(string sourceArchivePath)
+    {
+        byte[] archiveBytes = File.ReadAllBytes(sourceArchivePath);
+        char encryptionFlag = (char)archiveBytes[0];
+        byte[] actualData = archiveBytes.Skip(1).ToArray();
+
+        if (encryptionFlag == 'E')
+        {
+            var passwordDialog = new PasswordPromptForm();
+            var result = passwordDialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(passwordDialog.Password))
+            {
+                return DecryptWithPassword(actualData, passwordDialog.Password);
+            }
+            else
+            {
+                throw new OperationCanceledException("Password not provided for encrypted archive.");
+            }
+        }
+
+        return actualData; 
     }
 }
