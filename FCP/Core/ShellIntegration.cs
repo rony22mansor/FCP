867bb9098c8d87003c4f0a4212d7101d20f0b5e0
MarioAndra <marioandrawos75@gmail.com>
2025-06-28 03:32:26 +0300

    Right menu support

--- FCP/Core/ShellIntegration.cs
+++ FCP/Core/ShellIntegration.cs
@@ -6,18 +6,12 @@ using System.Windows.Forms;
 
 namespace FCP.Core
 {
-    /// <summary>
-    /// Manages the integration of the application with Windows Shell context menu.
-    /// </summary>
     public static class ShellIntegration
     {
         private const string AppName = "FCPCompressor";
         private const string FileTypeProgId = "FCP.ArchiveFile";
         private const string FileExtension = ".fcp";
 
-        /// <summary>
-        /// Registers context menu entries for files, folders, and .fcp files.
-        /// </summary>
         public static void Register()
         {
             if (!IsAdministrator())
@@ -30,7 +24,7 @@ namespace FCP.Core
             {
                 string appPath = Application.ExecutablePath;
 
-                // 1. Register the custom file extension (.fcp)
+                // 1. Register the custom .fcp extension
                 using (RegistryKey extKey = Registry.ClassesRoot.CreateSubKey(FileExtension))
                     extKey.SetValue("", FileTypeProgId);
 
@@ -49,21 +43,19 @@ namespace FCP.Core
                     }
                 }
 
-                // 2. Add "Compress with FCP" for all files (except .fcp)
+                // 2. Add "Compress with FCP" for all FILES
                 using (RegistryKey key = Registry.ClassesRoot.CreateSubKey($@"*\shell\{AppName}Compress"))
                 {
                     key.SetValue("", "Compress with FCP");
-                    key.SetValue("AppliesTo", "System.FileExtension:<>\"" + FileExtension + "\"");
-
+                    // Removes restriction to allow all file types (not just non-.fcp)
                     using (RegistryKey command = key.CreateSubKey("command"))
                         command.SetValue("", $"\"{appPath}\" \"%1\"");
                 }
 
-                // 3. Add "Compress with FCP" for folders
+                // 3. Add "Compress with FCP" for FOLDERS
                 using (RegistryKey key = Registry.ClassesRoot.CreateSubKey($@"Directory\shell\{AppName}Compress"))
                 {
                     key.SetValue("", "Compress with FCP");
-
                     using (RegistryKey command = key.CreateSubKey("command"))
                         command.SetValue("", $"\"{appPath}\" \"%1\"");
                 }
@@ -78,9 +70,6 @@ namespace FCP.Core
             }
         }
 
-        /// <summary>
-        /// Unregisters all context menu entries.
-        /// </summary>
         public static void Unregister()
         {
             if (!IsAdministrator())
@@ -131,8 +120,7 @@ namespace FCP.Core
             }
             catch
             {
-                // User declined UAC
-                return;
+                return; // User cancelled UAC
             }
 
             Application.Exit();
